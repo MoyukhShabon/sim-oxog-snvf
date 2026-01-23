3c7c3085903bc56513556ac6b6695c102582048f
MoyukhShabon <moyukh.work@gmail.com>
2026-01-23 20:19:23 +0800

    added ref genome path to setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 home="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 bin=$home/bin
-ref=$home/ref/tp53_hg38.fasta
+ref=$home/data/ref/tp53_hg38.fasta
 annot=$home/annot
 
 mkdir -p $annot
